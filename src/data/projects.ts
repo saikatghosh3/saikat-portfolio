6ed37a7f1ae5aad6a220db663b9927e5151bafb3
Saikat Ghosh <saikat.blast20@gmail.com>
2025-02-19 01:18:28 +0600

    edited

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,14 +3,25 @@ import image1 from '../assets/E-commerce.jpg';
 import image2 from '../assets/Business.jpg';
 import image3 from '../assets/Landing page.jpg';
 import image4 from '../assets/Travel.jpg';
+import image5 from '../assets/Healthcare.jpg.jpg';
+import image6 from '../assets/MovieApp.jpg.jpg';
+import image7 from '../assets/Universiy.jpg.jpg';
 
 export const projects: Project[] = [
+  // {
+  //   title: "E-Commerce Platform",
+  //   description: "A full-featured e-commerce platform with cart functionality and payment integration",
+  //   technologies: ["React", "TypeScript", "Tailwind CSS", "Redux"],
+  //   imageUrl: image1, 
+  //   liveUrl: "https://modernshop-e-commerce-website.vercel.app/",
+  //   githubUrl: "https://github.com"
+  // },
   {
-    title: "E-Commerce Platform",
-    description: "A full-featured e-commerce platform with cart functionality and payment integration",
+    title: "HealthCare",
+    description: "A full-featured Healthcare platform with patient portal and payment integration",
     technologies: ["React", "TypeScript", "Tailwind CSS", "Redux"],
-    imageUrl: image1, 
-    liveUrl: "https://modernshop-e-commerce-website.vercel.app/",
+    imageUrl: image5, 
+    liveUrl: "https://react-healthcare.vercel.app/",
     githubUrl: "https://github.com"
   },
   {
@@ -18,25 +29,41 @@ export const projects: Project[] = [
     description: "A business portfolio website with a modern design and smooth animations",
     technologies: ["React", "TypeScript", "Animations", "Tailwind CSS"],
     imageUrl: image2,
-    liveUrl: "https://portfolio-website-business.netlify.app/",
+    liveUrl: "https://business-portfolio-website-responsive.vercel.app/",
     githubUrl: "https://github.com"
   },
   {
-    title: "Landing Page Template",
-    description: "A responsive landing page template with a clean design and reusable components",
-    technologies: ["React", "Gsap", "Tailwind css", "TypeScript", "vite"],
-    imageUrl: image3,
-    liveUrl: "https://landing-page-design-eight-topaz.vercel.app/",
+    title: "Movie app",
+    description: "A responsive Movie app  with a clean design and reusable components",
+    technologies: ["React",  "Tailwind css", "TypeScript", "vite"],
+    imageUrl: image6,
+    liveUrl: "https://react-movie-app-seven-pi.vercel.app/genre/35",
     githubUrl: "https://github.com"
   },
+  // {
+  //   title: "Landing Page Template",
+  //   description: "A responsive landing page template with a clean design and reusable components",
+  //   technologies: ["React", "Gsap", "Tailwind css", "TypeScript", "vite"],
+  //   imageUrl: image3,
+  //   liveUrl: "https://landing-page-design-eight-topaz.vercel.app/",
+  //   githubUrl: "https://github.com"
+  // },
   {
-    title: "Travel and turism website", 
-    description: "A travel and tourism website with a booking system and user authentication",
-    technologies: ["React", "TypeScript", "Redux", "Styled Components", "Firebase"],
-    imageUrl: image4,
-    liveUrl: "https://destination-travel-theta.vercel.app/",
+    title: "University website",
+    description: "A responsive University website with a clean code and  design",
+    technologies: ["Html", "Css", "JavaScript",],
+    imageUrl: image7,
+    liveUrl: "https://university-website-responsive.vercel.app/",
     githubUrl: "https://github.com"
   }
+  // {
+  //   title: "Travel and turism website", 
+  //   description: "A travel and tourism website with a booking system and user authentication",
+  //   technologies: ["React", "TypeScript", "Redux", "Styled Components", "Firebase"],
+  //   imageUrl: image4,
+  //   liveUrl: "https://destination-travel-theta.vercel.app/",
+  //   githubUrl: "https://github.com"
+  // }
   // {
   //   title: "Weather Dashboard",
   //   description: "Interactive weather dashboard with real-time updates and historical data visualization",
