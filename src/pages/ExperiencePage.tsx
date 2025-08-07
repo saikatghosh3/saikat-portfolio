cd63b7cf33a26746d39c4ea00057a87f79c16bcc
Saikat Ghosh <saikat.blast20@gmail.com>
2025-08-08 02:39:42 +0600

    fixed-responsive

--- src/pages/ExperiencePage.tsx
+++ src/pages/ExperiencePage.tsx
@@ -3,27 +3,9 @@ import { motion } from 'framer-motion';
 import { Timeline } from '../components/Timeline';
 import { experiences } from '../data/experience';
 
-// export const ExperiencePage = () => {
-//   return (
-//     <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-20">
-//       <div className="text-center mb-16">
-//         <h1 className="text-4xl font-bold mb-2">Professional Experience</h1>
-//     <motion.div
-//   initial={{ width: 0 }}
-//   animate={{ width: '80%' }}
-//   transition={{ duration: 0.6, ease: 'easeOut' }}
-//   className="h-1 bg-blue-500 mx-auto rounded max-w-xs sm:max-w-md md:max-w-lg"
-// />
-
-//       </div>
-
-//       <Timeline experiences={experiences} />
-//     </div>
-//   );
-// };
-
-
-<div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-20 overflow-x-hidden">
+export const ExperiencePage = () => {
+  return (
+ <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-20 overflow-x-hidden">
   <div className="text-center mb-16">
     <h1 className="text-4xl font-bold mb-2">Professional Experience</h1>
     <motion.div
@@ -36,4 +18,9 @@ import { experiences } from '../data/experience';
 
   <Timeline experiences={experiences} />
 </div>
+  );
+};
+
+
+
 
