0d8960344493a4b8b22f60e07b469e3e80ea935e
Saikat Ghosh <saikat.blast20@gmail.com>
2026-08-31 00:07:14 +0600

    add new project images and update designProjects data; include corruption news website and Zilian

--- src/data/designProjects.ts
+++ src/data/designProjects.ts
@@ -4,11 +4,11 @@ import image20 from '../assets/apexfootball.png';
 import image21 from '../assets/sikkhadan.png';
 import image22 from '../assets/smartHisab.png';
 import image23 from '../assets/typely.png';
-
-
+import image24 from '../assets/corruptionbd24.png';
+import image25 from '../assets/zilian.png';;
 export const designProjects: Project[] = [
   {
-    title: "Business portfolio",
+    title: "Typely Application",
     description: "A business portfolio website with a modern design and smooth animations",
     technologies: ["React", "TypeScript", "Animations", "Tailwind CSS"],
     imageUrl: image23,
@@ -16,7 +16,7 @@ export const designProjects: Project[] = [
     githubUrl: "https://github.com",
   },
   {
-    title: "Movie app",
+    title: "Smart Hisab",
     description: "A responsive Movie app with a clean design and reusable components",
     technologies: ["React", "Tailwind css", "TypeScript", "vite"],
     imageUrl: image22,
@@ -24,7 +24,7 @@ export const designProjects: Project[] = [
     githubUrl: "https://github.com",
   },
   {
-    title: "Landing Page Template",
+    title: "Sikkha Daan",
     description: "A responsive landing page template with a clean design and reusable components",
     technologies: ["React", "Gsap", "Tailwind css", "TypeScript", "vite"],
     imageUrl: image21,
@@ -32,11 +32,27 @@ export const designProjects: Project[] = [
     githubUrl: "https://github.com",
   },
   {
-    title: "University website",
+    title: "Apex Football",
     description: "A responsive University website with a clean code and design",
-    technologies: ["Html", "Css", "JavaScript"],
+    technologies: ["Next.js", "Tailwind css", "TypeScript"],
     imageUrl: image20,
     liveUrl: "https://tournament-website-phi.vercel.app/",
+    githubUrl: "https://github.com",
+  },
+  {
+    title: "corruption news website",
+    description: "A responsive University website with a clean code and design",
+    technologies: ["Next.js", "Tailwind css", "TypeScript"],
+    imageUrl: image24,
+    liveUrl: "https://corruption-newsbd24.netlify.app/",
+    githubUrl: "https://github.com",
+  },
+  {
+    title: "Zilian",
+    description: "A responsive website with a clean code and design",
+    technologies: ["Next.js", "Tailwind css", "TypeScript"],
+    imageUrl: image25,
+    liveUrl: "https://zilian.netlify.app/",
     githubUrl: "https://github.com",
   },
   
